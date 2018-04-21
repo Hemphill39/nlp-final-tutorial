6f071b4a196a5ca70773119f23557e60d3a75d63
Amad881 <amadh881@gmail.com>
2018-04-21 16:14:56 -0400

    Added comments to polarity graph

--- polarity_graph.py
+++ polarity_graph.py
@@ -34,13 +34,18 @@ for new_word in but_relationships:
         if seed_word in neg_words:
             distance_matrix[new_word][seed_word] += 1
 
+#Import the following to classify the new seed words
 from sklearn.naive_bayes import GaussianNB
 import numpy as np
 
+#Create a square matrix of size seedCt x seedCt to insert training samples
 allSeeds = pos_words + neg_words
 seedCt = len(allSeeds)
 trainMatrix = np.zeros((seedCt, seedCt))
+#Create vector of length seedCt to label training features
 trainScores = np.zeros(seedCt)
+
+#Populate train matrix with positive seed words
 for rowA, word in enumerate(pos_words):
     if word in distance_matrix:
         columns = distance_matrix[word]
@@ -48,6 +53,7 @@ for rowA, word in enumerate(pos_words):
             columnNum = allSeeds.index(seedWord)
             trainMatrix[rowA][columnNum] = columns[seedWord]
 
+#Populate train matrix with negative seed words
 rowA += 1
 for rowB, word in enumerate(neg_words):
     if word in distance_matrix:
@@ -57,19 +63,24 @@ for rowB, word in enumerate(neg_words):
             trainMatrix[rowA+rowB][columnNum] = columns[seedWord]
     trainScores[rowA+rowB] = 1
 
+#Ensure every seed word correlates to itself
 np.fill_diagonal(trainMatrix, 1)
 
+#Train a naive bayes classifier on the seed words
 clf = GaussianNB()
 clf.fit(trainMatrix, trainScores)
 
+#Parse through each new word
 for word in distance_matrix.keys():
     if word not in allSeeds:
         row = np.zeros(seedCt)
         columns = distance_matrix[word]
+        #Load in each seed word's features into a vector of length == seedCt
         for seedWord in columns.keys():
             columnNum = allSeeds.index(seedWord)
             row[columnNum] = columns[seedWord]
         row = [list(row)]
+        #Classify the new word and determine if it is positive, negative or neutral based on its score
         scores = clf.predict_proba(row)[0]
         diff = scores[0] - scores[1]
         threshold = 0.1
@@ -78,6 +89,7 @@ for word in distance_matrix.keys():
         elif diff < (threshold*(-1)):
             neg_words.append(word)
 
+#Print out the new and old seeds
 with open("new_pos_words.txt",'w') as fout:
     for word in pos_words:
         fout.write(str(word) + "\n")
